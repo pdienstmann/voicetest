4acb3ae001c4aeb2ba84f666fd9f2dfa1e03f68c
Pietro Dienstmann <110914538+pdienstmann@users.noreply.github.com>
2024-05-31 20:39:54 -0300

    Add files via upload

---
+++ app/validacao.js
@@ -0,0 +1,23 @@
+function verificaSeOChutePossuiUmValorValido(chute){
+
+    if(chute == "Sim"){
+        document.body.innerHTML=`<h2>Uhuuuuuul 🥳</h2>
+                                <h3></h3>
+                                <img src="./frame.png">`;
+    } else if(chute == "Não") {
+        elementoChute.innerHTML += ` <div>:(</div>
+        <button id="jogar-novamente" class="btn-jogar">Tentar novamente</button>`;
+    
+    } else {
+        elementoChute.innerHTML += ` <div>Meus IFs não têm essa resposta, tenta sim ou não 😂 kkkkkkkkk</div>
+        <button id="jogar-novamente" class="btn-jogar">Tentar novamente</button>`;
+    }
+
+}
+
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload();
+    }
+})
